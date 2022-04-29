337d3b55c910fb2953dbcdd283bd5c524d9dfb4c
MarcoAraoz <marcojuarezaraoz@gmail.com>
2022-04-29 16:07:40 -0500

    Clase 6 de python

--- clase 6_martes 19 de abril/exercises.py
+++ clase 6_martes 19 de abril/exercises.py
@@ -2,16 +2,14 @@
 # [1,4,9,10,23]
 # 9.4
 
-
-
-# lista = [1,4,9,10,23]
-# suma = sum(lista)/len(lista)
-# print(suma)
+lista = [1,4,9,10,23]
+promedio = sum(lista)/len(lista)
+print(promedio)
 
 #Excercise 2
 #Remover una sublista de una lista
-l1= [1,4,9,10,23]
-l2= [4,9]
+#l1= [1,4,9,10,23]
+#l2= [4,9]
 #[1, 10, 23]
 
 from typing import List
@@ -31,4 +29,18 @@ new_list = remove_list(l1, l2)
 print(new_list)
 
 
+#Crear una lista que devuelva los cuadrados de los primeros 
+#10 números del rango del 1 al 10
+#[1,2,3,4,5,6,7,8,9,10]
+#[1,4,9,16,25,36,49,64,81,100]
+
+def gen_cuadrados(n):
+    cuadrados = []
+
+    for number in range(1, n +1):
+        cuadrados.append(number **2)
+    return cuadrados
+
+resultado = gen_cuadrados(10)
+print(resultado)
 
